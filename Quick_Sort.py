1367f2c907bed9873f1c8a4aee4ba172fe4b4ecb
jayzobalia <81687946+jayzobalia@users.noreply.github.com>
2022-04-18 14:40:15 +0530

    Add files via upload

---
+++ Quick_Sort.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+
+def QuickSort(arr):
+    elements = len(arr)
+
+    if elements < 2:
+        return arr
+
+    current_position = 0
+
+    for i in range(1, elements):
+        if arr[i] <= arr[0]:
+            current_position += 1
+            temp = arr[i]
+            arr[i] = arr[current_position]
+            arr[current_position] = temp
+
+    temp = arr[0]
+    arr[0] = arr[current_position]
+    arr[current_position] = temp
+
+    left = QuickSort(arr[0:current_position])
+    right = QuickSort(arr[current_position + 1:elements])
+
+    arr = left + [arr[current_position]] + right
+
+    return arr
+
+
+array_to_be_sorted = np.random.randint(0,1000,5)
+print("Unsorted Array: ", array_to_be_sorted)
+print("Sorted Array: ", QuickSort(array_to_be_sorted))
+
+
